107151a5f6ecb8024864ac58eaa4d8bb85ebc3dd
Luana Ueno <108813631+luaueno@users.noreply.github.com>
2024-05-09 16:15:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const dataAtual = new Date ();
-let dataObjetivo = prompt("Data inal (formato ANO-MÊS-DIA, exemplo: 2024-05-30");
-dataObjetivo = new Date(dataObjetivo="T23:59:59");
+let dataObjetivo = prompt("Data final (formato ANO-MÊS-DIA, exemplo: 2024-05-30");
+dataObjetivo = new Date(dataObjetivo+"T23:59:59");
 let diasQueFaltam = Math.floor ((dataObjetivo - dataAtual) / 86400000);
-document.querySelector("#dias_restantes").textContent = diasQueFaltam=" dias";
+document.querySelector("#dias_restantes").textContent = diasQueFaltam+" dias";
